02ac967406c354aa336bd8220c93dd55d822a5ac
Jamie C. Driver <jamie@blockstream.io>
2022-09-28 14:09:31 +0100

    fix lbtc input and change output in test_send_amp_confidential_pset()

--- tests/test_transaction_service.py
+++ tests/test_transaction_service.py
@@ -91,28 +91,30 @@ async def test_send_amp_confidential_pset():
     lockerSvc = await Locker.create()
     accountSvc = AccountService(session, lockerSvc)
     transactionSvc = TransactionService(session, lockerSvc)
-    fees_selection = transactionSvc.select_utxos(accountName, '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49', 10000)
-    amp_selection = transactionSvc.select_utxos(ampAccountName, 'bea126b86ac7f7b6fc4709d1bb1a8482514a68d35633a5580d50b18504d5c322', 1)
 
-    utxo = amp_selection.utxos[0]
-    fee_utxo = fees_selection.utxos[0]
+    FEE = 500
+    fees_selection = transactionSvc.select_utxos(accountName, '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49', FEE)
+    amp_selection = transactionSvc.select_utxos(ampAccountName, 'bea126b86ac7f7b6fc4709d1bb1a8482514a68d35633a5580d50b18504d5c322', 1)
+    amp_utxo = amp_selection.utxos[0]
     
     receiveAddr = 'vjTvPHdcJFZrYL9LmFpPootd1tmWqzugF9MXwhet6cdeCKEK6WJrb2mPEQGw7WNpikAoTq9ui22GU2pS' 
     unconf = wally.confidential_addr_to_addr(receiveAddr, wally.WALLY_CA_PREFIX_LIQUID_TESTNET)
     receiveScript = wally.address_to_scriptpubkey(unconf, wally.WALLY_NETWORK_LIQUID_TESTNET)
     blindingPubKey = wally.confidential_addr_to_ec_public_key(receiveAddr, wally.WALLY_CA_PREFIX_LIQUID_TESTNET)
 
-    pset = add_input_utxo(GdkAPI(session), transactionSvc.create_empty_pset(), utxo.gdk_utxo)
-    pset = add_input_utxo(GdkAPI(session), pset, fee_utxo.gdk_utxo)
-    pset = wally.psbt_from_base64(pset)
+    gdkAPI = GdkAPI(session)
+    psetb64 = add_input_utxo(gdkAPI, transactionSvc.create_empty_pset(), amp_utxo.gdk_utxo)
+    for utxo in fees_selection.utxos:
+        psetb64 = add_input_utxo(gdkAPI, psetb64, utxo.gdk_utxo)
 
+    pset = wally.psbt_from_base64(psetb64)
     amp_asset = Asset.from_hex('bea126b86ac7f7b6fc4709d1bb1a8482514a68d35633a5580d50b18504d5c322').to_bytes()
     lbtc = Asset.from_hex('144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49').to_bytes()
     
     output0 = wally.tx_elements_output_init(
         receiveScript,
         amp_asset,
-        wally.tx_confidential_value_from_satoshi(utxo.value),
+        wally.tx_confidential_value_from_satoshi(amp_utxo.value),
     )
     wally.psbt_add_tx_output_at(pset, 0, 0, output0)
     wally.psbt_set_output_blinding_public_key(pset, 0, blindingPubKey)
@@ -134,14 +136,14 @@ async def test_send_amp_confidential_pset():
     output_fee = wally.tx_elements_output_init(
         None,
         lbtc,
-        wally.tx_confidential_value_from_satoshi(1000),
+        wally.tx_confidential_value_from_satoshi(FEE),
     )
     wally.psbt_add_tx_output_at(pset, 2, 0, output_fee)
 
     b64 = wally.psbt_to_base64(pset, 0)
     blinded = transactionSvc.blind_pset(b64)
-    b64 = wally.psbt_to_base64(pset, 0)
     signed = transactionSvc.sign_pset(blinded)    
+    assert signed is not None
 
     if not wally.psbt_finalize(signed) == wally.WALLY_OK:
         raise Exception('Failed to finalize PSBT')
